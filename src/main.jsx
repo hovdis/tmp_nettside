a5a5378504248f8011069a61118a52912335f335
hovdis <rune.hovde@entur.org>
2024-04-30 16:06:54 +0200

    revert main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-class Main extends React.Component {
-  render() {
-    ReactDOM.createRoot(document.getElementById("root")).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-  }
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
